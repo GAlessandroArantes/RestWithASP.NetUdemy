7292a2b614d37f56d6f0833856af6752cf796e21
GAlessandroArantes <62617122+GAlessandroArantes@users.noreply.github.com>
2020-09-10 11:37:54 -0300

    Novas implementações

---
+++ RestWithASP.NetUdemyCalculator/RestWithASP.NetUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP.NetUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : Controller
+    {
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum (string firstNumber, string secondNumber)
+        {
+            if( IsNumeric(firstNumber) && IsNumeric(secondNumber) )
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return new OkObjectResult(sum.ToString());
+
+            }
+            return new BadRequestObjectResult("invalid input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if( decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string valorNumerico)
+        {
+            double number;
+            bool isNumber = double.TryParse(valorNumerico, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
